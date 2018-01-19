047eb02a5c647dfcd4596f1a692ce47e3c27962a
Karmov <Artur69Karmov@gmail.com>
2018-01-19 14:01:36 +0300

    Last commit

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse
-from .models import ProductInBasket
+from .models import *
 from django.shortcuts import render
+from .forms import CheckoutContactForm
+from django.contrib.auth.models import User
+
 
 def basket_adding(request):
     return_dict = dict()
@@ -36,6 +39,31 @@ def basket_adding(request):
 
 
 def checkout(request):
-    product_in_basket = ProductInBasket.objects.filter(session_key = request.session.session_key, is_active=True)
+    session_ley = request.session.session_key
+    product_in_basket = ProductInBasket.objects.filter(session_key = request.session.session_key, is_active=True).exclude(order__isnull=True)
+    form = CheckoutContactForm(request.POST or None)
+    if request.POST:
+        print(request.POST)
+        if form.is_valid():
+            print("yes")
+            data = request.POST
+            name = data.get("name", "user")
+            phone = data["phone"]
+            user, created = User.objects.get_or_create(username=phone, defaults={"first_name": name})
+            order = Order.objects.create(user=user, custom_name=name, custom_phone=phone, status_id=1)
+            for name, value in data.items():
+                if name.startswith("product_in_basket_"):
+                    product_in_basket_id = name.split("product_in_basket_")[1]
+                    product_in_basket = ProductInBasket.objects.get(id=product_in_basket_id)
+                    product_in_basket.count = value
+                    product_in_basket.order = order
+                    product_in_basket.save(force_update=True)
+                    ProductInOrder.objects.create(product=product_in_basket.product,
+                                                  count=product_in_basket.count,
+                                                  price_per_item=product_in_basket.price_per_item,
+                                                  total_price=product_in_basket.total_price,
+                                                  order=order)
+        else:
+            print("no")
     return render(request, 'orders/checkout.html', locals())
 
